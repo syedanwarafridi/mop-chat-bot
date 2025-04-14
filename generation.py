cb691adce2f57b2bde59d591042662d1b4bd3bbb
Syed Anwar <66356493+syedanwarafridi@users.noreply.github.com>
2025-04-14 15:26:55 +0500

    Update generation.py

--- generation.py
+++ generation.py
@@ -1,28 +1,35 @@
-from inference import load_fine_tuned_model, inference
-import argparse, os
-
+from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
+import os
 from dotenv import load_dotenv
+from inference import load_fine_tuned_model, inference
+from classifier import classifier_model
 
 load_dotenv()
 
-model_id = os.getenv('MODEL_ID')
-classifier_model = os.getenv('CLASSIFIER_MODEL')
+# -----> Fastapi Setup <----- #
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    model_id = os.getenv("MODEL_ID")
+    model, tokenizer = load_fine_tuned_model(model_id)
+    app.state.model = model
+    app.state.tokenizer = tokenizer
+    yield
+    del app.state.model
+    del app.state.tokenizer
 
-model = AutoModelForCausalLM.from_pretrained(
-    classifier_model,
-    torch_dtype="auto",
-    device_map="auto",
-)
-tokenizer = AutoTokenizer.from_pretrained(classifier_model)
+app = FastAPI(lifespan=lifespan)
 
+# -----> MOP-Bot Response Generation API <----- #
+@app.get("/bot-response", summary="Generate Bot Response", response_description="The generated response from the model.")
+async def get_bot_response(request: Request, query: str):
+    model = request.app.state.model
+    tokenizer = request.app.state.tokenizer
+    response = inference(model, tokenizer, query)
+    return {"response": response}
 
-model, tokenizer = load_fine_tuned_model(model_id)
-
-parser = argparse.ArgumentParser(description="Generate responses using a fine-tuned language model.")
-parser.add_argument('-i', '--input', type=str, required=True, help="Input text for the model to process.")
-args = parser.parse_args()
-
-    # Generate and print the response
-response = inference(model, tokenizer, args.input)
-print("\nModel Response:")
-print(response)
+# -----> query Classification Response API <----- #
+@app.get("/classifier-response", summary="Classifier Response", response_description="The generated response from the classifier.")
+async def get_classifier_response(query: str):
+    classification = classifier_model(query)
+    return {"classification": classification}
