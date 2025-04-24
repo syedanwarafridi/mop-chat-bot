013e18772133a98cf47b701baefea99febef961d
Syed Anwar <66356493+syedanwarafridi@users.noreply.github.com>
2025-04-24 19:11:09 +0500

    Update retriver.py

--- retriver.py
+++ retriver.py
@@ -1,5 +1,8 @@
 import requests
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # -----------------------> Similarity/Distance BASE API <----------------------- #
 def distance_api(query: str):
@@ -17,7 +20,7 @@ def distance_api(query: str):
             return {"error": f"Error {response.status_code}: {response.text}"}
     except requests.exceptions.RequestException as e:
         return {"error": f"Request failed: {e}"}
-
+    
 # -----------------------> Similarity/Distance BASE API <----------------------- #
 def token_api(query: str):
     url = "http://mop.rekt.life/v1/search"
@@ -28,11 +31,44 @@ def token_api(query: str):
             return response.json()
         else:
             # return {"error": f"Request failed with status code {response.status_code}", "details": response.text}
-            return {"data": f"Data is not available for the token."}
+            return {"data": f" "}
     
     except requests.exceptions.RequestException as e:
         return {"error": "Request exception occurred", "details": str(e)}
     
+# -----------------------> Last Update API <----------------------- #
+def last_update_api():
+    url = "https://mop.rekt.life/v1/update/crypto_assets"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+        if isinstance(data, dict) and data.get("success") and isinstance(data.get("data"), list) and len(data["data"]) == 2:
+            coinmarketcap_update = data["data"][0].get("last_update", "N/A")
+            solana_tracker_update = data["data"][1].get("last_update", "N/A")
+            return f"Last Update for Coinmarketcap: {coinmarketcap_update}, \nLast Update for SolanaTracker: {solana_tracker_update}"
+        else:
+            return {"error": "Unexpected JSON structure or missing data."}
+    except requests.exceptions.RequestException as e:
+        return {"error": "Request exception occurred", "details": str(e)}
+    except ValueError:
+        return {"error": "Failed to parse JSON response."}
+    
+# -------------------------> Update Data API <----------------------- #
+def update_api():
+    url = "https://mop.rekt.life/v1/update/crypto_assets"
+    try:
+        response = requests.post(url)
+        
+        if response.status_code == 200:
+            
+            return response.json()
+        else:
+            return {"error": f"Request failed with status code {response.status_code}", "details": response.text}
+    
+    except requests.exceptions.RequestException as e:
+        return {"error": "Request exception occurred", "details": str(e)}
 
 # -----------------------> Tavily API <----------------------- #
 from langchain_community.tools.tavily_search import TavilySearchResults
@@ -41,7 +77,7 @@ tavily_api_key = os.getenv('TAVILY_API_KEY')
 
 os.environ['TAVILY_API_KEY'] = tavily_api_key
 def tavily_data(query: str):
-    tool = TavilySearchResults(max_results=5)
+    tool = TavilySearchResults(max_results=5,include_domains=["https://crypto.news/", "https://cointelegraph.com/"], include_images=False, include_videos=False, include_links=True)
     # tools = [tool]
     results = tool.invoke(query)
     filtered_results = [{"title": item["title"], "content": item["content"]} for item in results]
